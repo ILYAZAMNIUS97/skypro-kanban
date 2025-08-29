bf6d11f2973c765a05fa8ea8b518414fe9ca0ad0
ILYAZAMNIUS97 <zamnius.ilya@yandex.ru>
2025-08-29 19:55:08 +0300

    Реализация новой страницы 404

--- src/pages/NotFoundPage/NotFoundPage.styled.js
+++ src/pages/NotFoundPage/NotFoundPage.styled.js
@@ -8,6 +8,7 @@ export const PageWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  font-family: "Roboto", Arial, Helvetica, sans-serif;
 `;
 
 export const NotFoundContainer = styled.div`
@@ -18,46 +19,78 @@ export const NotFoundContainer = styled.div`
   justify-content: center;
 `;
 
-export const NotFoundContent = styled.div`
+export const NotFoundModal = styled.div`
+  width: 368px;
+  height: auto;
+  background-color: #ffffff;
+  border-radius: 10px;
+  border: 0.7px solid #d4dbe5;
+  box-shadow: 0px 4px 67px -12px rgba(0, 0, 0, 0.13);
+  padding: 40px 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
-  padding: 40px;
+`;
+
+export const NotFoundContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 `;
 
 export const NotFoundTitle = styled.h1`
-  font-size: 120px;
+  font-size: 64px;
   font-weight: 700;
   color: #565eef;
   margin-bottom: 20px;
   line-height: 1;
+  letter-spacing: -1.28px;
 `;
 
 export const NotFoundSubtitle = styled.h2`
-  font-size: 32px;
-  font-weight: 600;
+  font-size: 20px;
+  font-weight: 700;
   color: #000;
   margin-bottom: 20px;
+  line-height: 30px;
+  letter-spacing: -0.6px;
 `;
 
 export const NotFoundText = styled.p`
-  font-size: 16px;
-  color: #94a6be;
-  margin-bottom: 40px;
-  line-height: 1.5;
+  font-size: 14px;
+  color: #000;
+  margin-bottom: 30px;
+  line-height: 150%;
+  letter-spacing: -0.14px;
+  max-width: 280px;
 `;
 
 export const NotFoundButton = styled.button`
+  width: 100%;
+  height: 30px;
   background-color: #565eef;
-  color: white;
-  border: none;
   border-radius: 4px;
-  padding: 12px 20px;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   font-size: 14px;
+  line-height: 1;
   font-weight: 500;
+  letter-spacing: -0.14px;
+  color: #ffffff;
   cursor: pointer;
   text-decoration: none;
-  display: inline-block;
+  font-family: "Roboto", Arial, Helvetica, sans-serif;
 
   &:hover {
+    background-color: #33399b;
+  }
+
+  &:active {
     background-color: #33399b;
   }
 `;
